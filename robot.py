ea9a99723be2565db3d4a5e7ea8b0e0da67e85a1
dawsonbowhay <awsumdawson2@gmail.com>
2016-02-09 16:35:58 -0800

    Slight syntax fix

--- robot.py
+++ robot.py
@@ -22,9 +22,9 @@ class MainRobot (wpilib.IterativeRobot):
         self.BL.setPID(.5, 0.0, 4.0, 0.0)
         self.BR.setPID(.5, 0.0, 4.0, 0.0)
 
-        self.MoveJoy = joystickLib.createJoystick(0)
+        self.MoveJoy = JoystickLib.joystickLib.createJoystick(0)
         self.MoveJoy.invertY()
-        self.TurnJoy = joystickLib.createJoystick(1)
+        self.TurnJoy = JoystickLib.joystickLib.createJoystick(1)
         self.TurnJoy.invertY()
 
         self.Drive = HolonomicDrive(self.FL, self.FR, self.BL, self.BR)
