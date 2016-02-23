21df799529739a98f6a3138043c769dd67e462ca
Jacob van't Hoog <vantjac@gmail.com>
2016-02-23 15:34:01 -0800

    Add to arm test robot.py

--- Arm/robot.py
+++ Arm/robot.py
@@ -1,12 +1,15 @@
 __author__ = "jacobvanthoog"
 
 import wpilib
+from .ArmControl import ArmControl
 
 class TestArmEncoders (wpilib.IterativeRobot):
 
     def robotInit(self):
         self.Arm1 = wpilib.CANTalon(6)
         self.Arm2 = wpilib.CANTalon(7)
+        self.Control = ArmControl(self.Arm1, self.Arm2)
+        self.Control.moveToPosition(20, 17.5)
     def autonomousPeriodic(self):
         pass
 
@@ -15,6 +18,7 @@ class TestArmEncoders (wpilib.IterativeRobot):
 
     def teleopPeriodic(self):
         print(self.Arm1.getEncPosition(), "   ", self.Arm2.getEncPosition())
+        #self.Control.update()
 
 if __name__ == "__main__":
     wpilib.run(TestArmEncoders)
