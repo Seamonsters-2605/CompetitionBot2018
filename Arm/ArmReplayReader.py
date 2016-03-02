e58fd70631762bb4e8aa0f14e5d46ebabd3a16d2
Jacob van't Hoog <vantjac@gmail.com>
2016-03-02 09:45:28 -0800

    Complete ArmReplayReader.py

--- Arm/ArmReplayReader.py
+++ Arm/ArmReplayReader.py
@@ -2,7 +2,8 @@ __author__ = "jacobvanthoog"
 
 class ArmReplayReader:
 
-    def __init__(self, filePath):
+    def __init__(self, replay, filePath):
+        self.Arm = replay
         file = open(filePath)
         lines = file.readlines()
         self.Positions = [None for i in range(0, len(lines))]
@@ -13,5 +14,36 @@ class ArmReplayReader:
             self.Positions[i] = position
             i += 1
         print(self.Positions)
+        self.resetPath()
+        self.disable()
 
-arm = ArmReplayReader("/Users/Jacob van't Hoog/testPath.txt")
+    def resetPath(self):
+        self.Point = 0 # index in self.Positions
+        self.Started = False
+
+    def enable(self):
+        self.Enabled = True
+
+    def disable(self):
+        self.Disabled = False
+
+    def update(self):
+        if not self.Enabled:
+            return
+        
+        if not self.Started:
+            self.Arm.setTarget(self.Positions[self.Point])
+            return
+
+        if self.Arm.movementCompleted():
+            print("Point", self.Point, "reached!")
+            self.Point += 1
+            if self.Point == len(self.Positions):
+                self.disable()
+                self.resetPath()
+                return
+            self.Arm.setTarget(self.Positions[self.Point])
+
+        return
+
+arm = ArmReplayReader(None, "/Users/Jacob van't Hoog/testPath.txt")
