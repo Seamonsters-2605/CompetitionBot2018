31a3fd8b1adff8c2e7f4ed69d3d5d4f87a54d876
Jacob van't Hoog <vantjac@gmail.com>
2016-05-17 16:21:18 -0700

    Add library folder and drive interface. No implementation yet.

---
+++ seamonsters/drive.py
@@ -0,0 +1,20 @@
+__author__ = "jacobvanthoog"
+
+class DriveInterface:
+
+    class DriveMode:
+        VOLTAGE = 1
+        SPEED = 2
+        POSITION = 3
+    
+    def __init__(self):
+        self.driveMode = DriveInterface.DriveMode.SPEED
+
+    def setDriveMode(self, mode):
+        self.driveMode = mode
+
+    def getDriveMode(self):
+        return self.driveMode
+    
+    def drive(self, magnitude, direction, turn):
+        pass
