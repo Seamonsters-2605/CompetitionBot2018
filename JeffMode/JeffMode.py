061762dc625e131f733035c98ebaf20d8f741d5c
Sehome Seamonsters <frc2605@gmail.com>
2016-05-24 22:54:15 +0000

    JeffMode uses getPosition() instead of getEncPosition()

--- JeffMode/JeffMode.py
+++ JeffMode/JeffMode.py
@@ -9,14 +9,14 @@ class JeffMode:
 
         if not (self.Talon.getControlMode() == wpilib.CANTalon.ControlMode.Position):
             self.Talon.changeControlMode(wpilib.CANTalon.ControlMode.Position)
-        self.encoderTarget = self.Talon.getEncPosition() #zero encoder targets
+        self.encoderTarget = self.Talon.getPosition() #zero encoder targets
         self.invert = 1 # can be 1 or -1
         self.maxVelocity = 80 * 5
 
 
     def set(self, magnitude): #Increments position to mock speed mode
         store = magnitude * self.maxVelocity
-        if not abs(self.Talon.getEncPosition() - self.encoderTarget) > 250: #Started @ 1000
+        if not abs(self.Talon.getPosition() - self.encoderTarget) > 250: #Started @ 1000
             self.encoderTarget += store * self.invert
         self.Talon.set(self.encoderTarget)
     
