240a89527b03ac8e5b0e5126564894debacc7504
Sehome Seamonsters <frc2605@gmail.com>
2016-06-09 19:26:13 +0000

    Add features to deploy.sh and test.sh: search for different python aliases, allow nonstandard robot file

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,24 @@
-python3 robot.py deploy --builtin --nc
+file=robot.py
+
+if [ $# == 1 ]
+    then
+        file=$1
+fi
+
+echo Deploying robot $file
+
+hash python3 2> /dev/null || {
+    hash python 2> /dev/null || {
+        hash py 2> /dev/null || {
+            echo "Python not found!"
+            exit 1
+        }
+        py $file deploy --builtin --nc --nonstandard
+        exit 0
+    }
+    python $file deploy --builtin --nc --nonstandard
+    exit 0
+}
+python3 $file deploy --builtin --nc --nonstandard
+echo "Done, press enter to quit"
 read
