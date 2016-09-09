67abe161e985fab8680e205e69fcc9563a6800de
Sehome Seamonsters <frc2605@gmail.com>
2016-09-09 20:51:16 +0000

    Fix SwerveBot using SwerveBotDrive

--- swerveBot.py
+++ swerveBot.py
@@ -8,7 +8,7 @@ class SwerveBot(Module):
     
     def __init__(self):
         super().__init__()
-        self.addModule(SwerveBotDrive)
+        self.addModule(SwerveBotDrive())
         
 if __name__ == "__main__":
     wpilib.run(SwerveBot)
