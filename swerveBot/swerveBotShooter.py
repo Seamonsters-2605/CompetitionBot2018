f1793a431663a9e7e51302fed7a606e314b810fd
Sehome Seamonsters <frc2605@gmail.com>
2016-09-19 15:35:15 +0000

    Write SwerveBotShooter based on robot.py

--- swerveBot/swerveBotShooter.py
+++ swerveBot/swerveBotShooter.py
@@ -1,16 +1,30 @@
 __author__ = "jacobvanthoog"
 
 from seamonsters.wpilib_sim import simulate
+from seamonsters.gamepad import Gamepad
+import Shooter.ShootController
 import wpilib
 
 class SwerveBotShooter(wpilib.IterativeRobot):
     
     def robotInit(self):
-        pass
-            
+        self.gamepad = Gamepad(1)
+        
+        self.LeftFly = wpilib.CANTalon(4)
+        self.RightFly = wpilib.CANTalon(5)
+        self.LimitSwitch = wpilib.DigitalInput(0)
+        self.LimitSwitch2 = wpilib.DigitalInput(1)
+        self.Intake = wpilib.CANTalon(8)
+        self.Shooter = Shooter.ShootController.ShootController(\
+            self.LeftFly, self.RightFly,\
+            self.Intake, self.LimitSwitch, self.LimitSwitch2)
+        
     def teleopPeriodic(self):
-        pass
+        self.Shooter.update(self.gamepad.getRawButton(Gamepad.B),\
+                            self.gamepad.getRawButton(Gamepad.X),\
+                            self.gamepad.getRawButton(Gamepad.A),\
+                            self.gamepad.getRawButton(Gamepad.Y))
         
         
 if __name__ == "__main__":
-    wpilib.run(SwerveBotDrive)
+    wpilib.run(SwerveBotShooter)
