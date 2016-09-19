09fd782a419dd732212508479b976f1520a6fbce
Jacob van't Hoog <vantjac@gmail.com>
2016-09-19 14:04:48 -0700

    Update with new port numbers

--- swerveBot/swerveBotDrive.py
+++ swerveBot/swerveBotDrive.py
@@ -11,34 +11,34 @@ class SwerveBotDrive(DriveTest):
     def robotInit(self):
         super().robotInit()
         
-        # Front Right wheel
-        frDrive = wpilib.CANTalon(2)
-        frDrive.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
-        frDrive.setPID(1.0, 0.0, 3.0, 0.0)
+        # Front Left wheel
+        flDrive = wpilib.CANTalon(0)
+        flDrive.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
+        flDrive.setPID(1.0, 0.0, 3.0, 0.0)
         
-        frRotate = wpilib.CANTalon(1)
-        frRotate.reverseOutput(True)
-        frRotate.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
-        frRotate.setPID(1.0, 0.0, 3.0, 0.0)
+        flRotate = wpilib.CANTalon(1)
+        flRotate.reverseOutput(True)
+        flRotate.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
+        flRotate.setPID(1.0, 0.0, 3.0, 0.0)
         
         
-        # Back Left wheel
-        blDrive = wpilib.CANTalon(3)
-        blDrive.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
-        blDrive.setPID(1.0, 0.0, 3.0, 0.0)
+        # Back Right wheel
+        brDrive = wpilib.CANTalon(2)
+        brDrive.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
+        brDrive.setPID(1.0, 0.0, 3.0, 0.0)
         
-        blRotate = wpilib.CANTalon(0)
-        blRotate.reverseOutput(True)
-        blRotate.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
-        blRotate.setPID(1.0, 0.0, 3.0, 0.0)
+        brRotate = wpilib.CANTalon(3)
+        brRotate.reverseOutput(True)
+        brRotate.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
+        brRotate.setPID(1.0, 0.0, 3.0, 0.0)
         
         # Drive controller
         drive = SwerveDrive()
         
         # 104 gear teeth / 18 gear teeth * 280 ticks per rotation * 4 (quad)
         # then divide by 2 for some reason
-        drive.addWheel(1.0, 1.0, frDrive, frRotate, -104/18*280*4/2)
-        drive.addWheel(1.0, -1.0, blDrive, blRotate, -104/18*280*4/2)
+        drive.addWheel(-1.0, 1.0, flDrive, flRotate, -104/18*280*4/2)
+        drive.addWheel(1.0, -1.0, brDrive, brRotate, -104/18*280*4/2)
         
         DriveTest.initDrive(self, drive,
             driveMode=DriveInterface.DriveMode.VOLTAGE)
