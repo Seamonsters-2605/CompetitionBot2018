547a15e5e0033cf7883ab8dc010c03c7e8e496c2
Sehome Seamonsters <frc2605@gmail.com>
2016-10-03 15:29:55 +0000

    Add encoder test for swerve bot

--- testBot.py
+++ testBot.py
@@ -1,16 +1,17 @@
 __author__ = "jacobvanthoog"
 
-from seamonsters.gamepad import Gamepad
 import wpilib
 
 class Test(wpilib.IterativeRobot):
     def robotInit(self):
         wpilib.SmartDashboard.putString("thisIsAKey", "this is a value!")
-        self.Gamepad = Gamepad(0)
+        self.Talon1 = wpilib.CANTalon(1)
+        self.Talon1.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
+        self.Talon2 = wpilib.CANTalon(3)
+        self.Talon2.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
 
     def teleopPeriodic(self):
-        print(self.Gamepad.getLX(), self.Gamepad.getLY(),
-              self.Gamepad.getRX(), self.Gamepad.getRY())
+        print(self.Talon1.getPosition(), self.Talon2.getPosition())
         
 if __name__ == "__main__":
     wpilib.run(Test)
