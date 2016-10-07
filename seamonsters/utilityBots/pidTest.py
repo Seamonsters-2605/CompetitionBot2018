835b5d47edfc31c57ec5d96645da71128b35d154
Jacob van't Hoog <vantjac@gmail.com>
2016-10-07 14:05:13 -0700

    Easier to read logging in pidTest, easier to switch adjusted value

--- seamonsters/utilityBots/pidTest.py
+++ seamonsters/utilityBots/pidTest.py
@@ -1,16 +1,12 @@
 __author__ = 'Dawson'
+
 import wpilib
+import seamonsters.joystick
 
 class PIDTest(wpilib.IterativeRobot):
 
     def robotInit(self, talonPort, ticksPerRotation=4000, maxVelocity=10):
-        print("PID Adjustor")
-        print("Move the joystick up and down to spin the motor")
-        print("Press 4 to toggle the selected value (P, I, D, F)")
-        print("Use 2 and 3 to increase/decrease the selected value")
-        print("Hold trigger to print the speed of the motor continuously")
-        print()
-        print("Using CANTalon:", talonPort)
+        self.talonPort = talonPort
         
         self.Talon = wpilib.CANTalon(talonPort)
         self.Talon.setPID(1,0,1,0)
@@ -21,7 +17,7 @@ class PIDTest(wpilib.IterativeRobot):
         self.maxVelocity = maxVelocity
         self.ticksPerRotation = ticksPerRotation
 
-        self.Joystick= wpilib.Joystick(0)
+        self.Joystick = seamonsters.joystick.JoystickUtils(0)
         self.JoystickValues = [0,0,0,0]
 
         self.PIDNumber = 0
@@ -29,16 +25,26 @@ class PIDTest(wpilib.IterativeRobot):
         self.buttonAllowedTime = 0
 
     def teleopInit(self):
+        print("PID Adjustor")
+        print("Move the joystick up and down to spin the motor")
+        print("Press 4 to toggle the selected value (P, I, D, F)")
+        print("Use 2 and 3 to increase/decrease the selected value")
+        print("Hold trigger to print the speed of the motor continuously")
+        print()
+        print("Using CANTalon:", self.talonPort)
+        
         self.goalPosition = self.Talon.getEncPosition()
     
     # DO NOT CHANGE TELEOPPERIODIC, CHANGE DOALLELSE
     def teleopPeriodic(self):
+        self.Joystick.updateButtons()
+        
         if (self.buttonAllowedTime <= self.loopCalls):
-            if self.Joystick.getRawButton(4):
+            if self.Joystick.buttonPressed(4):
                 self.PIDNumber += 1
                 if self.PIDNumber > 3:
                     self.PIDNumber = 0
-                print("Currently adjusting: ")
+                print("Currently adjusting: ", end="")
                 if self.PIDNumber == 0:
                     print("P")
                 elif self.PIDNumber == 1:
