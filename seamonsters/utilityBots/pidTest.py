6384c74b03c7cce4d2af8b95c95258a6f0865240
Jacob van't Hoog <vantjac@gmail.com>
2016-10-07 14:13:42 -0700

    Easier adjusting of PID values for pidTest

--- seamonsters/utilityBots/pidTest.py
+++ seamonsters/utilityBots/pidTest.py
@@ -21,8 +21,6 @@ class PIDTest(wpilib.IterativeRobot):
         self.JoystickValues = [0,0,0,0]
 
         self.PIDNumber = 0
-        self.loopCalls = 0
-        self.buttonAllowedTime = 0
 
     def teleopInit(self):
         print("PID Adjustor")
@@ -39,44 +37,36 @@ class PIDTest(wpilib.IterativeRobot):
     def teleopPeriodic(self):
         self.Joystick.updateButtons()
         
-        if (self.buttonAllowedTime <= self.loopCalls):
-            if self.Joystick.buttonPressed(4):
-                self.PIDNumber += 1
-                if self.PIDNumber > 3:
-                    self.PIDNumber = 0
-                print("Currently adjusting: ", end="")
-                if self.PIDNumber == 0:
-                    print("P")
-                elif self.PIDNumber == 1:
-                    print("I")
-                elif self.PIDNumber == 2:
-                    print("D")
-                elif self.PIDNumber == 3:
-                    print("F")
-                self.buttonAllowedTime = self.loopCalls + 5
-            
-            elif self.Joystick.getRawButton(3):
-                self.changePID(1.1)
-                self.printValues()
-                self.buttonAllowedTime = self.loopCalls + 5
-                
-            elif self.Joystick.getRawButton(2):
-                self.changePID(1/1.1)
-                self.printValues()
-                self.buttonAllowedTime = self.loopCalls + 5
-                
-            elif self.Joystick.getRawButton(1):
-                print("Speed: " + str(self.Talon.getVelocity()))
-                self.buttonAllowedTime = self.loopCalls + 5
-
+        if self.Joystick.buttonPressed(4):
+            self.PIDNumber += 1
+            if self.PIDNumber > 3:
+                self.PIDNumber = 0
+            print("Currently adjusting: ", end="")
+            if self.PIDNumber == 0:
+                print("P")
+            elif self.PIDNumber == 1:
+                print("I")
+            elif self.PIDNumber == 2:
+                print("D")
+            elif self.PIDNumber == 3:
+                print("F")
+                    
+        elif self.Joystick.buttonPressed(3):
+            self.changePID(1.1)
+            self.printValues()
+        
+        elif self.Joystick.buttonPressed(2):
+            self.changePID(1.0/1.1)
+            self.printValues()
+        
+        elif self.Joystick.getRawButton(1):
+            print("Speed: " + str(self.Talon.getVelocity()))
+        
         if not (abs(self.goalPosition - self.Talon.getPosition()) \
                 > self.ticksPerRotation):
             self.goalPosition += self.Joystick.getY() * -1 * self.maxVelocity
         self.Talon.set(self.goalPosition)
-
-        #AT END
-        self.loopCalls += 1
-        self.loopCalls %= 1000000
+        
         
     def printValues(self):
         print("P: " + str(self.Talon.getP()) \
