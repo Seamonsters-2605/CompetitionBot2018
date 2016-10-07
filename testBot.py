2e6b36ad754b69fd14782bbb3a7cd6e2480738b0
Sehome Seamonsters <frc2605@gmail.com>
2016-10-07 20:41:57 +0000

    testBot is PIDTest

--- testBot.py
+++ testBot.py
@@ -1,25 +1,13 @@
 __author__ = "jacobvanthoog"
 
 from seamonsters.gamepad import Gamepad
+from seamonsters.utilityBots.pidTest import PIDTest
 import wpilib
 
-class Test(wpilib.IterativeRobot):
-    def robotInit(self):
-        wpilib.SmartDashboard.putString("thisIsAKey", "this is a value!")
-        self.Talon1 = wpilib.CANTalon(1)
-        self.Talon1.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
-        self.Talon2 = wpilib.CANTalon(3)
-        self.Talon2.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
-        self.Gamepad = Gamepad(0)
-
-    def teleopPeriodic(self):
-        #print(self.Talon1.getPosition(), self.Talon2.getPosition())
-        print(self.Gamepad.getRawButton(Gamepad.UP),
-              self.Gamepad.getRawButton(Gamepad.DOWN),
-              self.Gamepad.getRawButton(Gamepad.LEFT),
-              self.Gamepad.getRawButton(Gamepad.RIGHT),
-              self.Gamepad.getRawButton(Gamepad.LT),
-              self.Gamepad.getRawButton(Gamepad.RT))
+class Test(PIDTest):
+    def __init__(self):
+        super().__init__(4)
+        #wpilib.SmartDashboard.putString("thisIsAKey", "this is a value!")
         
 if __name__ == "__main__":
     wpilib.run(Test)
