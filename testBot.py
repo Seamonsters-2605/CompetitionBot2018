e8109a4013298728817196cca65182d1e2ee981b
Jacob van't Hoog <vantjac@gmail.com>
2016-11-20 21:57:04 -0800

    NavX test

--- testBot.py
+++ testBot.py
@@ -1,13 +1,23 @@
 __author__ = "jacobvanthoog"
 
-from seamonsters.gamepad import Gamepad
-from seamonsters.utilityBots.pidTest import PIDTest
 import wpilib
+from robotpy_ext.common_drivers.navx import AHRS
+from seamonsters.logging import LogState
 
-class Test(PIDTest):
+class Test(wpilib.IterativeRobot):
+
     def robotInit(self):
-        super().robotInit(5)
+        self.ahrs = AHRS.create_spi() # the NavX
+        #self.ahrs = AHRS.create_i2c() # use this instead?
+        
+        self.yawLog = LogState("Yaw")
+        self.angleLog = LogState("Angle")
+        
         #wpilib.SmartDashboard.putString("thisIsAKey", "this is a value!")
+        
+    def teleopPeriodic(self):
+        self.yawLog.update(ahrs.getYaw())
+        self.angleLog.update(ahrs.getAngle())
         
 if __name__ == "__main__":
     wpilib.run(Test)
