9dcb9adba122366dafcb6f8440d9b6dc4b7ea370
Jacob van't Hoog <vantjac@gmail.com>
2016-12-14 14:09:15 -0800

    Use gamepads for PIDTest

--- seamonsters/utilityBots/pidTest.py
+++ seamonsters/utilityBots/pidTest.py
@@ -1,7 +1,7 @@
 __author__ = 'Dawson'
 
 import wpilib
-import seamonsters.joystick
+from seamonsters.gamepad import Gamepad
 
 class PIDTest(wpilib.IterativeRobot):
 
@@ -17,16 +17,17 @@ class PIDTest(wpilib.IterativeRobot):
         self.maxVelocity = maxVelocity
         self.ticksPerRotation = ticksPerRotation
 
-        self.Joystick = seamonsters.joystick.JoystickUtils(0)
-        self.JoystickValues = [0,0,0,0]
+        self.gamepad = Gamepad(0)
 
+        self.adjustFactor = 1.0
         self.PIDNumber = 0
 
     def teleopInit(self):
         print("PID Adjustor")
-        print("Hold trigger and move up or down to change the motor speed")
-        print("Press 4 to toggle the selected value (P, I, D, F)")
-        print("Use 2 and 3 to increase/decrease the selected value")
+        print("Hold RT and move the left joystick to change motor speed")
+        print("Press Start and Back to toggle the selected value (P, I, D, F)")
+        print("Press Left/Right to increase/decrease the adjust factor")
+        print("Press Up/Down to increase/decrease the selected value")
         print()
         print("Using CANTalon:", self.talonPort)
         print("Currently adjusting: P")
@@ -34,56 +35,68 @@ class PIDTest(wpilib.IterativeRobot):
         self.goalPosition = self.Talon.getPosition()
     
     def teleopPeriodic(self):
-        self.Joystick.updateButtons()
+        self.gamepad.updateButtons()
         
-        if self.Joystick.buttonPressed(4):
+        if self.gamepad.buttonPressed(Gamepad.START):
             self.PIDNumber += 1
             if self.PIDNumber > 3:
                 self.PIDNumber = 0
-            print("Currently adjusting: ", end="")
-            if self.PIDNumber == 0:
-                print("P")
-            elif self.PIDNumber == 1:
-                print("I")
-            elif self.PIDNumber == 2:
-                print("D")
-            elif self.PIDNumber == 3:
-                print("F")
-                    
-        elif self.Joystick.buttonPressed(3):
-            self.changePID(1.1)
-            self.printValues()
+            self._printAdjustedValue()
+        if self.gamepad.buttonPressed(Gamepad.BACK):
+            self.PIDNumber -= 1
+            if self.PIDNumber < 0:
+                self.PIDNumber = 3
+            self._printAdjustedValue()
         
-        elif self.Joystick.buttonPressed(2):
-            self.changePID(1.0/1.1)
-            self.printValues()
+        if self.gamepad.buttonPressed(Gamepad.LEFT):
+            self.adjustFactor /= 2.0
+            self._printAdjustFactor()
+        if self.gamepad.buttonPressed(Gamepad.RIGHT):
+            self.adjustFactor *= 2.0
+            self._printAdjustFactor()
+        
+        elif self.gamepad.buttonPressed(Gamepad.UP):
+            self._changePID(self.adjustFactor)
+            self._printValues()
+        elif self.gamepad.buttonPressed(Gamepad.DOWN):
+            self._changePID(-self.adjustFactor)
+            self._printValues()
 
-        if self.Joystick.getRawButton(1):
+        if self.gamepad.getRawButton(Gamepad.RT):
             if not (abs(self.goalPosition - self.Talon.getPosition()) \
                     > self.ticksPerRotation):
-                self.goalPosition += self.Joystick.getY() * -1 \
+                self.goalPosition += self.gamepad.getLY() * -1 \
                                      * self.maxVelocity
             self.Talon.set(self.goalPosition) 
             print("Speed: " + str(self.Talon.getEncVelocity()))
+    
         
-        
-    def printValues(self):
+    def _changePID(self, number):
+        if self.PIDNumber == 0:
+            self.Talon.setP(self.Talon.getP() + number)
+        elif self.PIDNumber == 1:
+            self.Talon.setI(self.Talon.getI() + number)
+        elif self.PIDNumber == 2:
+            self.Talon.setD(self.Talon.getD() + number)
+        elif self.PIDNumber == 3:
+            self.Talon.setF(self.Talon.getF() + number)
+
+    def _printAdjustedValue(self):
+        print("Currently adjusting: ", end="")
+        if self.PIDNumber == 0:
+            print("P")
+        elif self.PIDNumber == 1:
+            print("I")
+        elif self.PIDNumber == 2:
+            print("D")
+        elif self.PIDNumber == 3:
+            print("F")
+
+    def _printAdjustFactor(self):
+        print("Factor:", self.adjustFactor)
+
+    def _printValues(self):
         print("P: " + str(self.Talon.getP()) \
            + " I: " + str(self.Talon.getI()) \
            + " D: " + str(self.Talon.getD()) \
            + " F: " + str(self.Talon.getF()))
-        
-    def changePID(self, number):
-        if self.PIDNumber == 0:
-            self.Talon.setP(self._multiplyOrSet(self.Talon.getP(), number))
-        elif self.PIDNumber == 1:
-            self.Talon.setI(self._multiplyOrSet(self.Talon.getI(), number))
-        elif self.PIDNumber == 2:
-            self.Talon.setD(self._multiplyOrSet(self.Talon.getD(), number))
-        elif self.PIDNumber == 3:
-            self.Talon.setF(self._multiplyOrSet(self.Talon.getF(), number))
-
-    def _multiplyOrSet(self, old, factor):
-        if old == 0:
-            return factor
-        return old * factor
