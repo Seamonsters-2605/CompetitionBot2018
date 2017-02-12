ec135c5f5d082d2d8333b2052e2be68f5c58c74f
Jacob van't Hoog <vantjac@gmail.com>
2017-02-12 10:28:29 -0800

    Add getSwitch

--- seamonsters/dashboard.py
+++ seamonsters/dashboard.py
@@ -2,6 +2,25 @@ __author__ = "jacobvanthoog"
 
 import wpilib
 
+def getSwitch(name, defaultValue):
+    """
+    Get whether a switch on the dashboard is enabled.
+    """
+    table = NetworkTables.getTable('dashboard')
+    try:
+        switchNames = table.getStringArray('switchnames')
+        switchValues = table.getBooleanArray('switchvalues')
+    except BaseException as e:
+        print("Exception while getting switch", e)
+        return defaultValue
+    if not name in switchNames:
+        print("Couldn't find switch name", name)
+        return defaultValue
+    if len(switchNames) != len(switchValues):
+        print("Invalid switch data!")
+        return defaultValue
+    return switchValues[switchNames.index(name)]
+
 class DashboardBoolean:
     """
     References a single boolean value in the SmartDashboard
