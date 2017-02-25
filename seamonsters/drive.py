949501335264d688b90d66207434ff57e48bbe42
Jacob van't Hoog <vantjac@gmail.com>
2017-02-25 10:43:38 -0800

    Add DynamicPIDDrive

--- seamonsters/drive.py
+++ seamonsters/drive.py
@@ -175,6 +175,68 @@ class FieldOrientedDrive(DriveInterface):
     def _getYawRadians(self):
         return - math.radians(self.ahrs.getYaw())
 
+
+class DynamicPIDDrive(DriveInterface):
+    """
+    Wraps another drive interface. Based on the driving matnitude and turn
+    speed, the PID's of a set of talons are changed. PID's are given as a tuple
+    of (p, i, d, f). For speeds below ``slowPIDScale``, ``slowPID`` is used.
+    For speeds above ``fastPIDScale``, ``fastPID`` is used. For speeds in
+    between, the P/I/D/F values are interpolated.
+    """
+
+    def __init__(self, interface, wheelTalons, slowPID, slowPIDScale,
+                 fastPID, fastPIDScale, pidLookBackRange=10):
+        super().__init__()
+        self.interface = interface
+        self.talons = wheelTalons
+        self.slowPID = slowPID
+        self.slowPIDScale = slowPIDScale
+        self.fastPID = fastPID
+        self.fastPIDScale = fastPIDScale
+
+        self.currentPID = None
+        self.driveScales = [0.0 for i in range(0, pidLookBackRange)]
+
+    def setDriveMode(self, mode):
+        self.interface.setDriveMode(mode)
+
+    def getDriveMode(self):
+        return self.interface.getDriveMode()
+
+    def drive(self, magnitude, direction, turn, forceDriveMode=None):
+        driveScale = max(abs(magnitude), abs(turn * 2))
+        self.driveScales.append(driveScale)
+        self.driveScales.pop(0)
+        self._setPID(self._lerpPID(max(self.driveScales)))
+
+        self.interface.drive(magnitude, direction, turn, forceDriveMode)
+
+    def _setPID(self, pid):
+        if pid == self.currentPID:
+            return
+        self.currentPID = pid
+        for talon in self.talons:
+            talon.setPID(pid[0], pid[1], pid[2], pid[3])
+
+    def _lerpPID(self, magnitude):
+        if magnitude <= self.slowPIDScale:
+            return self.slowPID
+        elif magnitude >= self.fastPIDScale:
+            return self.fastPID
+        else:
+            # 0 - 1
+            scale = (magnitude - self.slowPIDScale) / \
+                    (self.fastPIDScale - self.slowPIDScale)
+            pidList = []
+            for i in range(0, 4):
+                slowValue = self.slowPID[i]
+                fastValue = self.fastPID[i]
+                value = (fastValue - slowValue) * scale + slowValue
+                pidList.append(value)
+            return tuple(pidList)
+
+
 if __name__ == "__main__":
     # test acceleration filter drive
     filterDrive = AccelerationFilterDrive(TestDriveInterface())
