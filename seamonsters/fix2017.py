01d71a843ef593dc7d8228278e2ae41ba061d901
Jacob van't Hoog <vantjac@gmail.com>
2017-11-22 11:37:45 -0800

    Remove fix2017.py

--- seamonsters/fix2017.py
+++
@@ -1,9 +0,0 @@
-__author__ = "jacobvanthoog"
-
-# This is a quick and temporary solution for code that was written before
-# CANTalons moved from wpilib to robotpy-ctre.
-
-import wpilib
-import ctre
-
-wpilib.CANTalon = ctre.CANTalon
