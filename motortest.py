1166c4681bcc9247060bbbfb632f2114d87b8cde
Jacob van't Hoog <vantjac@gmail.com>
2018-01-19 20:24:36 -0800

    Update motortest for 2018 ctre

--- motortest.py
+++ motortest.py
@@ -5,17 +5,11 @@ from networktables import NetworkTables
 
 class MotorTestBot(sea.GeneratorBot):
 
-    VALUE = 4
-    P = 0
-    I = 1
-    D = 2
-    F = 3
-    NUM_PARAMETERS = 5
-
     def robotInit(self):
-        self.talons = [ctre.CANTalon(i) for i in range(0,8)]
+        self.talons = [ctre.WPI_TalonSRX(i) for i in range(1,8)]
         for talon in self.talons:
-            talon.setFeedbackDevice(ctre.CANTalon.FeedbackDevice.QuadEncoder)
+            talon.configSelectedFeedbackSensor(
+                ctre.FeedbackDevice.QuadEncoder, 0, 0)
 
         self.statusLog = sea.LogState("!Motor Tester!")
         self.selectedLog = sea.LogState("Selected Motor")
@@ -35,7 +29,6 @@ class MotorTestBot(sea.GeneratorBot):
         self.statusLog.update("Running!")
 
         self.selectedI = 0
-        self.selectedParameter = MotorTestBot.VALUE
         self.commandReader.reset()
         self.setValue = 0
 
@@ -46,21 +39,9 @@ class MotorTestBot(sea.GeneratorBot):
 
             if self.joy.getRawButton(1):
                 while self.joy.getRawButton(1):
-                    control = -self.joy.getY() * (self.joy.getTwist() + 1) / 2
-                    mode = talon.getControlMode()
-                    if mode == ctre.CANTalon.ControlMode.PercentVbus:
-                        self.setValue = control
-                    elif mode == ctre.CANTalon.ControlMode.Position:
-                        pass # TODO
-                    elif mode == ctre.CANTalon.ControlMode.Speed:
-                        pass  # TODO
-                    elif mode == ctre.CANTalon.ControlMode.Current:
-                        pass  # TODO
-                    elif mode == ctre.CANTalon.ControlMode.Voltage:
-                        self.setValue = control * 12
-                    if self.setValue is not None:
-                        talon.enable()
-                        talon.set(self.setValue)
+                    self.setValue = \
+                        -self.joy.getY() * (self.joy.getTwist() + 1) / 2
+                    talon.set(ctre.ControlMode.PercentOutput, self.setValue)
                     self.updateTalonLog()
                     yield
                 self.setValue = None
@@ -77,43 +58,6 @@ class MotorTestBot(sea.GeneratorBot):
                     self.selectedI = 0
                 yield from self.buttonHeld(4)
 
-            if self.joy.getRawButton(3):
-                self.selectedParameter += 1
-                if self.selectedParameter >= MotorTestBot.NUM_PARAMETERS:
-                    self.selectedParameter = 0
-                yield from self.buttonHeld(3)
-            if self.joy.getRawButton(2):
-                self.selectedParameter -= 1
-                if self.selectedParameter < 0:
-                    self.selectedParameter = MotorTestBot.NUM_PARAMETERS - 1
-                yield from self.buttonHeld(2)
-
-            if self.joy.getRawButton(9):
-                talon.setControlMode((talon.getControlMode() + 1) % 8)
-                yield from self.buttonHeld(9)
-            if self.joy.getRawButton(8):
-                mode = talon.getControlMode() - 1
-                if mode < 0:
-                    mode = 7
-                talon.setControlMode(mode)
-                yield from self.buttonHeld(8)
-
-            command = self.commandReader.getCommand()
-            if command is not None:
-                value = float(command)
-                if self.selectedParameter == MotorTestBot.VALUE:
-                    self.setValue = value
-                    talon.enable()
-                    talon.set(self.setValue)
-                elif self.selectedParameter == MotorTestBot.P:
-                    talon.setP(value)
-                elif self.selectedParameter == MotorTestBot.I:
-                    talon.setI(value)
-                elif self.selectedParameter == MotorTestBot.D:
-                    talon.setD(value)
-                elif self.selectedParameter == MotorTestBot.F:
-                    talon.setF(value)
-
     def buttonHeld(self, buttonNum):
         while self.joy.getRawButton(buttonNum):
             self.updateTalonLog()
@@ -123,27 +67,27 @@ class MotorTestBot(sea.GeneratorBot):
         talon = self.talons[self.selectedI]
         self.selectedLog.update(talon.getDeviceID())
         self.modeLog.update(sea.talonModeToString(talon.getControlMode()))
-        self.outputVoltageLog.update(talon.getOutputVoltage())
+        self.outputVoltageLog.update(talon.getMotorOutputVoltage())
         self.outputCurrentLog.update(talon.getOutputCurrent())
-        self.encoderPositionLog.update(talon.getPosition())
-        self.encoderSpeedLog.update(talon.getSpeed())
+        self.encoderPositionLog.update(talon.getSelectedSensorPosition(0))
+        self.encoderSpeedLog.update(talon.getSelectedSensorVelocity(0))
 
-        pidf = (talon.getP(), talon.getI(), talon.getD(), talon.getF())
+        pidf = (talon.configGetParameter(
+                    ctre.WPI_TalonSRX.ParamEnum.eProfileParamSlot_P, 0, 0),
+                talon.configGetParameter(
+                    ctre.WPI_TalonSRX.ParamEnum.eProfileParamSlot_I, 0, 0),
+                talon.configGetParameter(
+                    ctre.WPI_TalonSRX.ParamEnum.eProfileParamSlot_D, 0, 0),
+                talon.configGetParameter(
+                    ctre.WPI_TalonSRX.ParamEnum.eProfileParamSlot_F, 0, 0))
         pidfStr = ""
         for i in range(0, 4):
-            if i == self.selectedParameter:
-                pidfStr += "["
             pidfStr += str(pidf[i])
-            if i == self.selectedParameter:
-                pidfStr += "]"
             if i != 3:
                 pidfStr += ","
         self.pidLog.update(pidfStr)
 
-        if self.selectedParameter == MotorTestBot.VALUE:
-            self.setLog.update('[' + str(self.setValue) + ']')
-        else:
-            self.setLog.update(self.setValue)
+        self.setLog.update(self.setValue)
         sea.sendLogStates()
 
 if __name__ == "__main__":
