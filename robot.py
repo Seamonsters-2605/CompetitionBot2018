d4a320c22b05a4641b85c36df8c186f545d37f15
Jacob van't Hoog <vantjac@gmail.com>
2018-02-07 10:03:21 -0800

    Add timer

--- robot.py
+++ robot.py
@@ -9,13 +9,23 @@ class MainRobot(sea.GeneratorBot):
         self.driveObject = drive.DriveBot.__new__(drive.DriveBot)
         drive.DriveBot.robotInit(self.driveObject)
         self.shooterInstance = sea.IterativeRobotInstance(shooter.MyRobot)
+        self.timerLogState = sea.LogState("Time")
+
+    def timer(self):
+        i = 0
+        while True:
+            i += 1
+            self.timerLogState.update(i // 50)
+            yield
 
     def teleop(self):
         yield from sea.parallel(drive.DriveBot.teleop(self.driveObject),
-                                self.shooterInstance.teleopGenerator())
+                                self.shooterInstance.teleopGenerator(),
+                                self.timer())
 
     def autonomous(self):
-        yield from drive.DriveBot.autonomous(self.driveObject)
+        yield from sea.parallel(drive.DriveBot.autonomous(self.driveObject),
+                                self.timer())
 
 if __name__ == "__main__":
     wpilib.run(MainRobot)
