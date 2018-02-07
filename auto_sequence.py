801af0afc86a306d7741eb95f112569b4e20831b
Warrono <wrrnrose@gmail.com>
2018-02-06 19:37:04 -0800

    Working (kinda) with starting/switch positions. Exceleration when strafing

--- auto_sequence.py
+++ auto_sequence.py
@@ -5,22 +5,53 @@ import strafe_to_align
 import auto_shoot
 import seamonsters as sea
 import rotation_align
+import wpilib
 from robotpy_ext.common_drivers.navx import AHRS
 
 def autoSequence(drive, vision):
-    for i in range(70):
+    switchPosition = wpilib.DriverStation.getInstance().getGameSpecificMessage()
+    if switchPosition[0] == "L":
+            switchPos1 = 75
+            switchPos2 = 120
+            switchPos3 = 244
+    if switchPosition[0] == "R":
+            switchPos1 = 244
+            switchPos2 = 75
+            switchPos3 = 75
+    for i in range(60):
         drive.drive(.3, math.pi/2, 0)
         yield
     drive.drive(0, 0, 0)
+   # yield from sea.watch(sea.untilTrue(auto_commands.watchWheelRotation(
+    #    drive.interface.wheelMotors[sea.HolonomicDrive.FRONT_RIGHT], 70)),
+     #                    auto_commands.driveContinuous(drive, .3, math.pi/2, 0))
     yield from sea.wait(25)
-    for i in range(75):
-        drive.drive(.3, 0, 0)
-        yield
-    drive.drive(0, 0, 0)
+    if wpilib.DriverStation.getInstance().getLocation() == 1:
+        for i in range(switchPos1):
+            drive.drive(.3, 0, 0)
+            yield
+        drive.drive(0, 0, 0)
+    elif wpilib.DriverStation.getInstance().getLocation() == 2:
+        if switchPos2 == 75:
+            for i in range(switchPos2):
+                drive.drive(i/150, 0, 0)
+                yield
+        elif switchPos2 == 120:
+            for i in range(switchPos2):
+                drive.drive(-i/240, 0, 0)
+                yield
+        drive.drive(0, 0, 0)
+    elif wpilib.DriverStation.getInstance().getLocation() == 3:
+        for i in range(switchPos3):
+            drive.drive(-.3, 0, 0)
+            yield
+        drive.drive(0, 0, 0)
     yield from sea.wait(25)
     yield from sea.ensureTrue(strafe_to_align.strafeAlign(drive, vision, 0), 20)
-    yield from sea.watch(sea.wait(50), strafe_to_align.strafeAlign(drive, vision, 0),
-                         auto_commands.driveContinuous(drive, .2, math.pi/2, 0))
+    drive.drive(0, 0, 0)
+    yield from sea.watch(strafe_to_align.strafeAlign(drive, vision, 0),
+                         auto_commands.driveContinuous(drive, .3, math.pi/2, 0), sea.wait(90))
+    drive.drive(0, 0, 0)
 
 def autonomous(drive, ahrs, vision):
     multiDrive = sea.MultiDrive(drive)
