55dfd0a64cfd0e47695bf65a98f566a47d56c348
Warrono <wrrnrose@gmail.com>
2018-02-09 15:07:11 -0800

    AAAAAAA some auto sequences now eliminated strafing. how cool

--- auto_sequence.py
+++ auto_sequence.py
@@ -53,31 +53,52 @@ def autoSequence(drive, vision):
         else:
             print("Invalid game message!")
             return
-        for i in range(60):
-            drive.drive(.3, math.pi/2, 0)
-            yield
-        drive.drive(0, 0, 0)
+
         yield from sea.wait(25)
         if wpilib.DriverStation.getInstance().getLocation() == 1:
-            for i in range(switchPos1):
-                drive.drive(.3, 0, 0)
-                yield
-            drive.drive(0, 0, 0)
+            if switchPosition[0] == "L":
+                for i in range(60):
+                    drive.drive(.3, math.pi / 2, 0)
+                    yield
+                drive.drive(0, 0, 0)
+                for i in range(switchPos1):
+                    drive.drive(.3, 0, 0)
+                    yield
+                drive.drive(0, 0, 0)
+            if switchPosition[0] == "R":
+                yield from sea.timeLimit(auto_driving.driveContinuous(drive, .3, 1.6, -1), 225)
+                yield from sea.timeLimit(auto_driving.driveContinuous(drive, .3, 1, 0.2), 30)
         elif wpilib.DriverStation.getInstance().getLocation() == 2:
             if switchPos2 == 75:
+                for i in range(60):
+                    drive.drive(.3, math.pi / 2, 0)
+                    yield
+                drive.drive(0, 0, 0)
                 for i in range(switchPos2):
                     drive.drive(i/150, 0, 0)
                     yield
             elif switchPos2 == 120:
+                for i in range(60):
+                    drive.drive(.3, math.pi / 2, 0)
+                    yield
+                drive.drive(0, 0, 0)
                 for i in range(switchPos2):
                     drive.drive(-i/240, 0, 0)
                     yield
             drive.drive(0, 0, 0)
         elif wpilib.DriverStation.getInstance().getLocation() == 3:
-            for i in range(switchPos3):
-                drive.drive(-.3, 0, 0)
-                yield
-            drive.drive(0, 0, 0)
+            if switchPosition[0] == "L":
+                yield from sea.timeLimit(auto_driving.driveContinuous(drive, .3, 1.6, 1), 230)
+                yield from sea.timeLimit(auto_driving.driveContinuous(drive, .3, 1, -0.2), 30)
+            if switchPosition[0] == "R":
+                for i in range(60):
+                    drive.drive(.3, math.pi / 2, 0)
+                    yield
+                drive.drive(0, 0, 0)
+                for i in range(switchPos3):
+                    drive.drive(-.3, 0, 0)
+                    yield
+                drive.drive(0, 0, 0)
         yield from sea.wait(25)
         yield from sea.ensureTrue(auto_vision.strafeAlign(drive, vision, 0), 20)
         drive.drive(0, 0, 0)
