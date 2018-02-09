47af15451e04e2d7e17d285adc419c4ceeb05d2d
Mila Brooks <mila.eris@gmail.com>
2018-02-09 11:58:34 -0800

    added current tests for the motors

--- lifter.py
+++ lifter.py
@@ -9,14 +9,35 @@ class Lifter():
         self.rightWing = ctre.WPI_TalonSRX(7)
         self.joystick = wpilib.Joystick(1)
 
-    def Lift(self):
-        if(self.joystick.getRawButton(9)):
-            self.leftWing.set(1)
-        if(self.joystick.getRawButton(10)):
-            self.rightWing.set(1)
-        else:
-            self.leftWing.set(0)
-            self.rightWing.set(0)
+    def teleopInit(self):        
+        self.leftWingRunning = True
+        self.rightWingRunning = True
+
+    def teleopPeriodic(self):
+        print(self.rightWing.getOutputCurrent())
+        print(self.leftWing.getOutputCurrent())
+
+        if(self.rightWing.getOutputCurrent() > 200):
+            self.rightWingRunning = False
+        if(self.leftWing.getOutputCurrent() > 200):
+            self.leftWingRunning = False
+
+        if(self.leftWingRunning):
+            if(self.joystick.getRawButton(9)):
+                self.leftWing.set(1)
+            else:
+                self.leftWing.set(0)
+
+        if(self.rightWingRunning):
+            if(self.joystick.getRawButton(10)):
+                self.rightWing.set(1)
+            else:
+                self.rightWing.set(0)
+
+        if(self.joystick.getRawButton(8)):
+            self.leftWingRunning = True
+            self.rightWingRunning = True
+
 
 if __name__ == "__main__":
     wpilib.run(Lifter, physics_enabled=True)
