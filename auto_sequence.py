73ec5ed05baf5b906907c0acca69840855d0b167
Mila Brooks <mila.eris@gmail.com>
2018-02-13 20:01:28 -0800

    simplified it, added angleHolder for orientation

--- auto_sequence.py
+++ auto_sequence.py
@@ -4,100 +4,53 @@ import auto_vision
 import seamonsters as sea
 import auto_navx
 import wpilib
+import auto_strategies
 
-def autoSequence(drive, vision):
+def autoSequence(drive, vision, angleHolder):
+    angleHolder[0] = 0
     switchPosition = wpilib.DriverStation.getInstance().getGameSpecificMessage()
-    if not sea.getSwitch("Activate Switch", False):
+    if not sea.getSwitch("Activate Switch", True):
         if wpilib.DriverStation.getInstance().getLocation() == 1:
-            for i in range(200):
-                drive.drive(.3, math.pi/2, 0)
-                yield
-            drive.drive(0, 0, 0)
+            yield from auto_strategies.left_cross(drive)
         elif wpilib.DriverStation.getInstance().getLocation() == 2:
-            for i in range(60):
-                drive.drive(.3, math.pi/2, 0)
-                yield
+            yield from auto_driving.driveDistance(drive, 50, .3)
+            yield
             drive.drive(0, 0, 0)
             if switchPosition[0] == "L":
-                for i in range(150):
-                    drive.drive(i/300, 0, 0)
-                    yield
-                drive.drive(0, 0, 0)
+                yield from auto_strategies.mid_cross_left(drive)
             elif switchPosition[0] == "R":
-                for i in range(200):
-                    drive.drive(-i/400, 0, 0)
-                    yield
-                drive.drive(0, 0, 0)
+                yield from auto_strategies.mid_cross_right(drive)
             for i in range(150):
                 drive.drive(.3, math.pi / 2, 0)
                 yield
             drive.drive(0, 0, 0)
         elif wpilib.DriverStation.getInstance().getLocation() == 3:
-            for i in range(200):
-                drive.drive(.3, math.pi/2, 0)
-                yield
-            drive.drive(0, 0, 0)
-    elif sea.getSwitch("Activate Switch", False):
+            yield from auto_strategies.right_cross(drive)
+    else:
         if len(switchPosition) == 0:
             print("No game message!")
             return
-        if switchPosition[0] == "L":
-                switchPos1 = 75
-                switchPos2 = 120
-                switchPos3 = 244
-        elif switchPosition[0] == "R":
-                switchPos1 = 244
-                switchPos2 = 75
-                switchPos3 = 75
-        else:
-            print("Invalid game message!")
-            return
+
 
         yield from sea.wait(25)
         if wpilib.DriverStation.getInstance().getLocation() == 1:
             if switchPosition[0] == "L":
-                for i in range(60):
-                    drive.drive(.3, math.pi / 2, 0)
-                    yield
-                drive.drive(0, 0, 0)
-                for i in range(switchPos1):
-                    drive.drive(.3, 0, 0)
-                    yield
-                drive.drive(0, 0, 0)
+                yield from auto_strategies.left_left(drive, angleHolder)
             if switchPosition[0] == "R":
-                yield from sea.timeLimit(auto_driving.driveContinuous(drive, .3, 1.6, -1), 225)
-                yield from sea.timeLimit(auto_driving.driveContinuous(drive, .3, 1, 0.2), 30)
+                yield from auto_strategies.left_right(drive, angleHolder)
+
         elif wpilib.DriverStation.getInstance().getLocation() == 2:
-            if switchPos2 == 75:
-                for i in range(60):
-                    drive.drive(.3, math.pi / 2, 0)
-                    yield
-                drive.drive(0, 0, 0)
-                for i in range(switchPos2):
-                    drive.drive(i/150, 0, 0)
-                    yield
-            elif switchPos2 == 120:
-                for i in range(60):
-                    drive.drive(.3, math.pi / 2, 0)
-                    yield
-                drive.drive(0, 0, 0)
-                for i in range(switchPos2):
-                    drive.drive(-i/240, 0, 0)
-                    yield
-            drive.drive(0, 0, 0)
+            if switchPosition[0] == "R":
+                yield from auto_strategies.mid_right(drive, angleHolder)
+            elif switchPosition[0] == "L":
+                yield from auto_strategies.mid_left(drive, angleHolder)
+
         elif wpilib.DriverStation.getInstance().getLocation() == 3:
             if switchPosition[0] == "L":
-                yield from sea.timeLimit(auto_driving.driveContinuous(drive, .3, 1.6, 1), 230)
-                yield from sea.timeLimit(auto_driving.driveContinuous(drive, .3, 1, -0.2), 30)
+                yield from auto_strategies.right_left(drive, angleHolder)
             if switchPosition[0] == "R":
-                for i in range(60):
-                    drive.drive(.3, math.pi / 2, 0)
-                    yield
-                drive.drive(0, 0, 0)
-                for i in range(switchPos3):
-                    drive.drive(-.3, 0, 0)
-                    yield
-                drive.drive(0, 0, 0)
+                yield from auto_strategies.right_right(drive, angleHolder)
+
         yield from sea.wait(25)
         yield from sea.ensureTrue(auto_vision.strafeAlign(drive, vision, 0), 20)
         drive.drive(0, 0, 0)
@@ -107,8 +60,9 @@ def autoSequence(drive, vision):
 
 def autonomous(drive, ahrs, vision, shooter):
     multiDrive = sea.MultiDrive(drive)
-    yield from sea.parallel(auto_navx.rotation(multiDrive, ahrs),
-                            autoSequence(multiDrive, vision), auto_driving.updateMultiDrive(multiDrive))
+    angleHolder = [0.0]
+    yield from sea.parallel(auto_navx.rotation(multiDrive, ahrs, angleHolder),
+                            autoSequence(multiDrive, vision, angleHolder), auto_driving.updateMultiDrive(multiDrive))
 
 
 def findTarget(vision, initialWait, timeLimit):
