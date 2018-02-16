ca7465f0e99266aa897362faa9c2721628dd0f0b
Warrono <wrrnrose@gmail.com>
2018-02-15 18:55:54 -0800

    works

--- auto_navx.py
+++ auto_navx.py
@@ -4,6 +4,6 @@ def rotation(drive, ahrs, angleHolder=None):
         angleHolder = [0.0]
     startAngle = ahrs.getAngle()
     while True:
-        offset = ahrs.getAngle() - angleHolder[0]
-        drive.drive(0, 0, offset/75)
+        offset = ahrs.getAngle() - startAngle - angleHolder[0]
+        drive.drive(0, 0, offset/100)
         yield
