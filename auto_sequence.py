e0569099641512827b6055ca2fc60e13bb6ee7ec
Mila Brooks <mila.eris@gmail.com>
2018-02-16 20:47:15 -0800

    small changes to distances

--- auto_sequence.py
+++ auto_sequence.py
@@ -9,11 +9,11 @@ import auto_strategies
 def autoSequence(drive, vision, angleHolder):
     angleHolder[0] = 0
     switchPosition = wpilib.DriverStation.getInstance().getGameSpecificMessage()
-    if not sea.getSwitch("Activate Switch", False):
+    if not sea.getSwitch("Activate Switch", True):
         if wpilib.DriverStation.getInstance().getLocation() == 1:
             yield from auto_strategies.left_cross(drive, angleHolder)
         elif wpilib.DriverStation.getInstance().getLocation() == 2:
-            yield from auto_driving.driveDistance(drive, 50, .3)
+            yield from auto_driving.driveDistance(drive, 35, .3)
             yield
             drive.drive(0, 0, 0)
             if switchPosition[0] == "L":
@@ -51,11 +51,11 @@ def autoSequence(drive, vision, angleHolder):
             if switchPosition[0] == "R":
                 yield from auto_strategies.right_right(drive, angleHolder)
 
-        yield from sea.wait(5)
+        yield from sea.wait(25)
         yield from sea.ensureTrue(auto_vision.strafeAlign(drive, vision, 0), 20)
         drive.drive(0, 0, 0)
         yield from sea.watch(auto_vision.strafeAlign(drive, vision, 0),
-                             auto_driving.driveDistance(drive, 40, .3))
+                             auto_driving.driveDistance(drive, 40, .5))
 
         drive.drive(0, 0, 0)
 
