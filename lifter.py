e64d67530d38d7f47da31d0c7f60b30d4a4a4c37
Jacob van't Hoog <vantjac@gmail.com>
2018-02-28 16:39:48 -0800

    New wing controls

--- lifter.py
+++ lifter.py
@@ -2,66 +2,73 @@ import wpilib
 import ctre
 import seamonsters as sea
 
-WING_SPEED = -.75 # clockwise
-MAX_WING_SPEED = -1.0
+WING_SPEED = .75
+MAX_WING_SPEED = 1.0
 CURRENT_LIMIT = 11
+WING_REVERSE = True # clockwise
 
 class Lifter(sea.GeneratorBot):
 
     def robotInit(self):
         self.leftWing = ctre.WPI_TalonSRX(6)
+        self.leftWing.configSelectedFeedbackSensor(
+            ctre.FeedbackDevice.QuadEncoder, 0, 0)
         self.rightWing = ctre.WPI_TalonSRX(7)
+        self.rightWing.configSelectedFeedbackSensor(
+            ctre.FeedbackDevice.QuadEncoder, 0, 0)
         try:
             self.driverJoystick
         except AttributeError:
             self.driverJoystick = wpilib.Joystick(0)
-        self.wingCurrentLog = sea.LogState("Wing Currents")
+        self.leftWingLog = sea.LogState("Wing L")
+        self.rightWingLog = sea.LogState("Wing R")
 
     def teleop(self):
-        leftWingRunning = True
-        rightWingRunning = True
+        yield from sea.parallel(
+            self.wingGenerator(self.leftWing, button=5, axis=4,
+                               axisReverse=1, log=self.leftWingLog),
+            self.wingGenerator(self.rightWing, button=4, axis=1,
+                               axisReverse=-1, log=self.rightWingLog))
+
+    def wingGenerator(self, motor, button, axis, axisReverse, log):
         try:
-            while True:
-                if leftWingRunning:
-                    current = self.leftWing.getOutputCurrent()
-                    leftCurrentStr = str(current)
-                    if current > CURRENT_LIMIT:
-                        self.leftWing.set(0)
-                        leftWingRunning = False
-                    elif self.driverJoystick.getRawButton(5):
-                        self.leftWing.set(WING_SPEED)
-                    elif self.driverJoystick.getRawButton(9):
-                        self.leftWing.set(MAX_WING_SPEED)
-                    else:
-                        self.leftWing.set(0)
-                else:
-                    self.leftWing.set(0)
-                    leftCurrentStr = "STOP"
-
-                if rightWingRunning:
-                    current = self.rightWing.getOutputCurrent()
-                    rightCurrentStr = str(current)
-                    if current > CURRENT_LIMIT:
-                        self.rightWing.set(0)
-                        rightWingRunning = False
-                    elif self.driverJoystick.getRawButton(4):
-                        self.rightWing.set(WING_SPEED)
-                    elif self.driverJoystick.getRawButton(3):
-                        self.rightWing.set(MAX_WING_SPEED)
-                    else:
-                        self.rightWing.set(0)
-                else:
-                    self.rightWing.set(0)
-                    rightCurrentStr = "STOP"
-
-                if self.driverJoystick.getRawButton(8):
-                    leftWingRunning = True
-                    rightWingRunning = True
-                self.wingCurrentLog.update(leftCurrentStr + " " + rightCurrentStr)
+            while not self.driverJoystick.getRawButton(button):
+                log.update("Ready to release")
                 yield
+            motor.set(WING_SPEED)
+            for _ in range(30):
+                log.update("Releasing")
+                yield
+            motor.set(0)
+            while self.driverJoystick.getRawButton(button):
+                log.update("Released")
+                yield
+            while True:
+                while not self.driverJoystick.getRawButton(button):
+                    log.update("Ready")
+                    yield
+                while self.driverJoystick.getRawButton(button):
+                    speed = WING_SPEED
+                    axisValue = self.driverJoystick.getRawAxis(axis) * axisReverse
+                    if axisValue > 0:
+                        speed += (1 - speed) * axisValue
+                    else:
+                        speed *= axisValue + 1
+                    motor.set(speed)
+                    current = motor.getOutputCurrent()
+                    log.update(current)
+                    if current > CURRENT_LIMIT:
+                        while self.driverJoystick.getRawButton(button):
+                            log.update("Current limit!")
+                            yield
+                        while not self.driverJoystick.getRawButton(button):
+                            log.update("Current limit!")
+                            yield
+                        break
+                    yield
+                motor.set(0)
         finally:
-            self.leftWing.set(0)
-            self.rightWing.set(0)
+            motor.set(0)
 
 
 if __name__ == "__main__":
