14e7c7d70da12bfa856599d2901d81fe86cff0ef
seamonsters <frc2605@gmail.com>
2018-03-13 14:59:55 -0700

    Control 2 led strips with one arduino

--- LEDControl/LEDControl.ino
+++ LEDControl/LEDControl.ino
@@ -1,43 +1,44 @@
 #include <Adafruit_NeoPixel.h>  //Library to control LED strip
 
-#define DATA_PIN 6      //Data pin to Arduino pin 6
+#define DATA_PIN_1 6      //Data pins for the LED strips
+#define DATA_PIN_2 7
 #define LED_NUM 28      //Number of leds per strip
 #define OFF 0
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LED_NUM, DATA_PIN, NEO_GRB + NEO_KHZ800);  //Create pixels object
+#define NUM_STRIPS 2
+
+Adafruit_NeoPixel pixels[] = {
+  Adafruit_NeoPixel(LED_NUM, DATA_PIN_1, NEO_GRB + NEO_KHZ800),  //Create pixels object
+  Adafruit_NeoPixel(LED_NUM, DATA_PIN_2, NEO_GRB + NEO_KHZ800)
+};
 
 // Color takes RGB values, from 0,0,0 up to 255,255,255
 // e.g. White = (255,255,255), Red = (255,0,0)
-int red = 0;
-int green = 0;
-int blue = 0;
 
 void setup(){
-  pixels.begin();             //Initializes the NeoPixel library
-  pixels.setBrightness(100);  //Value from 0 to 100%
+  for(int i = 0; i < NUM_STRIPS; i++) {
+    pixels[i].begin();             //Initializes the NeoPixel library
+    pixels[i].setBrightness(100);  //Value from 0 to 100%
+  }
 }
 
 void loop(){
-  AllAboard(0, 0, 255);
-}
-
-//Wing activation - light each LED sequentially
-void AllAboard(int red, int green, int blue){
   //LEDs light from 0 to LED_NUM-1
   for(int i = 0; i < LED_NUM; i++){
-    pixels.setPixelColor(i, pixels.Color(red, green, blue));
-    pixels.show();    //Send the current pixel color to the strip
+    pixels[0].setPixelColor(i, pixels[0].Color(0, 0, 255));
+    pixels[0].show();    //Send the current pixel color to the strip
+    pixels[1].setPixelColor(i, pixels[1].Color(0, 255, 0));
+    pixels[1].show();
     delay(30);        //Delay between each LED activation (ms)
   }  
   delay(300);  
 
   //Turn off all LEDs
-  for(int i = 0; i < LED_NUM; i++){
-    pixels.setPixelColor(i, pixels.Color(OFF, OFF, OFF));
-    pixels.show();
-  }  
+  for(int strip_i = 0; strip_i < NUM_STRIPS; strip_i++) {
+    for(int i = 0; i < LED_NUM; i++){
+      pixels[strip_i].setPixelColor(i, pixels[strip_i].Color(OFF, OFF, OFF));
+      pixels[strip_i].show();
+    }
+  }
 }
-
-
-
 
