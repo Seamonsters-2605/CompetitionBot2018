03a58c848591db189f0e8e3601d03fc7b8a8c5cc
Jacob van't Hoog <vantjac@gmail.com>
2018-04-06 22:48:07 -0700

    Blender: write markers to file

--- auto_scripting/blender_export.py
+++ auto_scripting/blender_export.py
@@ -6,11 +6,15 @@ scene = bpy.data.scenes['Scene']
 
 robot_obj = bpy.data.objects['TheRobot']
 
+markers = sorted(scene.timeline_markers, key=lambda m: m.frame)
+marker_i = 0
+
 with open("C:\\Temp\\Path.txt", "w") as file:
     for frame in range(scene.frame_start, scene.frame_end + FRAME_STEP, FRAME_STEP):
+        if marker_i < len(markers) and frame >= markers[marker_i].frame:
+            file.write("{} {}\n".format(markers[marker_i].frame, ":" + markers[marker_i].name))
+            marker_i += 1
         scene.frame_current = frame
         bpy.ops.object.paths_calculate()
         # frame number, x, y
-        line = "{} {} {}\n".format(frame, robot_obj.location[0], robot_obj.location[1])
-        print(line)
-        file.write(line)
+        file.write("{} {} {}\n".format(frame, robot_obj.location[0], robot_obj.location[1]))
