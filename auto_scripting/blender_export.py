d63e16c298c0e866352cfe047c531a08c0a9aba8
Tollboothslyr <kazp.entire@gmail.com>
2018-05-14 15:06:43 -0700

    added rotation, and curve tracking on export
    nothing tested to the max yet, and im creating a setup as we speak
    adds rotation on z axis and writes it, may have added too many brackets form file.write
    and add adds curves eval time which is 0 (start) to 100 (end)
    also will change dab to new name later

--- auto_scripting/blender_export.py
+++ auto_scripting/blender_export.py
@@ -5,16 +5,24 @@ FRAME_STEP = 25
 scene = bpy.data.scenes['Scene']
 
 robot_obj = bpy.data.objects['TheRobot']
-
+robot_path = bpy.data.curves('NurbsPath')
 markers = sorted(scene.timeline_markers, key=lambda m: m.frame)
 marker_i = 0
+#set frame
+dab = 0 #start pointe of curve. also keep marker_i and dab two different things.
+#when increasing from 100 to x end the curve end always needs to stay as >= 1 or <=100
+bpy.data.curves['NurbsPath'].eval_time = dab	#beggining of curve path thing
+
+print(robot_obj.rotation_euler) #debugging
 
 with open("C:\\Temp\\Path.txt", "w") as file:
     for frame in range(scene.frame_start, scene.frame_end + FRAME_STEP, FRAME_STEP):
         if marker_i < len(markers) and frame >= markers[marker_i].frame:
             file.write("{} {}\n".format(markers[marker_i].frame, ":" + markers[marker_i].name))
+            bpy.data.curves['NurbsPath'].eval_time = dab
             marker_i += 1
+            dab += 1            
         scene.frame_current = frame
         bpy.ops.object.paths_calculate()
-        # frame number, x, y
-        file.write("{} {} {}\n".format(frame, robot_obj.location[0], robot_obj.location[1]))
+        # frame number, x, y , z Rotation
+        file.write("{} {} {} {} {}\n".format(frame, robot_obj.location[0], robot_obj.location[1] robot_obj.rotation_euler[2])
