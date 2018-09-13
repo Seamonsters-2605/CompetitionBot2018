55b2d48fc4ef405b6465bd17db457c313203e311
Tollboothslyr <kazp.entire@gmail.com>
2018-09-13 13:41:08 -0700

    changed to work with blender 2.79
    and works with new* cool* laptops!!!!!

--- auto_scripting/blender_export.py
+++ auto_scripting/blender_export.py
@@ -2,7 +2,7 @@ import bpy
 #from setup import rotateWheels, 
 FRAME_STEP = 25
 #if rotateWheels = True:
-wheels_obj = bpy.data.objects['theWheels']
+#wheels_obj = bpy.data.objects['theWheels']
 #else:
  #   wheels_obj = " "
 scene = bpy.data.scenes['Scene']
@@ -19,18 +19,20 @@ marker_i = 0
 #bpy.data.objects['NurbsPath'].eval_time = pathTime	#beggining of curve path thing
 
 print(robot_obj.rotation_euler) #debugging
-robot_path.select = True
 robot_obj.select = True
-bpy.context.scene.objects.active = robot_path
-bpy.ops.object.parent_set(type='FOLLOW_PATH')
-robot_obj.constraints["FOLLOW_PATH"].use_curve_follow
+robot_path.select = True
+bpy.context.scene.objects.active = robot_obj
+#bpy.ops.object.parent_set(type='FOLLOW_PATH')
+empty = robot_path.constraints.new('FOLLOW_PATH')
+empty.target = robot_path
+#robot_obj.constraints["FOLLOW_PATH"].use_curve_follow
 K = robot_obj.constraints["Follow Path"] #or "FollowPathConstraint.use_curve_follow
 K.offset = 10 #arbitrary amount it worked when i did ten so im using ten
 K.influence = 0 #the new path time
 for obj in bpy.data.objects:
     obj.select = False
 
-with open("C:\\Temp\\Path.txt", "w") as file:
+with open("C:\\Users\**YOURNAME000***\Desktop\path.txt", "w") as file:
     for frame in range(scene.frame_start, scene.frame_end + FRAME_STEP, FRAME_STEP):
         bpy.ops.object.paths_calculate()
         if K.influence > 1:
