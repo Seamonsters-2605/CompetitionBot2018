861c95529046f757d213bb21b6e5d4abd06d288a
monkeyminion <kcil2000@yahoo.com>
2018-09-19 14:29:12 -0700

    Update coord_record.py

--- auto_scripting/coord_record.py
+++ auto_scripting/coord_record.py
@@ -2,17 +2,17 @@ import bpy, bmesh
 
 robot_obj = bpy.data.objects['TheRobot']
 
-robot_path = bpy.data.objects['NurbsPath']
+#robot_path = bpy.data.objects['NurbsPath']
 
 obj = bpy.context.active_object
 
 robot_obj.select = True
-
+data = obj.data
 bpy.context.scene.objects.active = robot_obj
 
 if obj.mode == 'EDIT':
     bm = bmesh.from_edit_mesh(obj.data)
-    vertices = bm.verts
+    vertices = bm.verts 
 
 else:
     vertices = obj.data.vertices
@@ -25,12 +25,18 @@ print(plain_verts)
 b = 0
 fe = 256
 increment = 25
+print( plain_verts)
 
-with open("C:\\Users\**YOURNAME000***\Desktop\path.txt", "w") as file:
-    for x in range(0,fe) and b in range(0,fe):
-    coords = plainverts[x]
-    coordsX = coords(1)
-    coordsY = coords(2)
-    b = b + increment
-    print(b, coordsX, coordsY)
-    file.write("{} {} {}\n".format(b,coordsX, coordsY)
+for x in range(0,len(data.vertices)):
+    coords = plain_verts[x]
+    coordsX = coords[0]
+    coordsY = coords[1]
+    print(coordsX,coordsY)
+
+with open("C:\\Users\TANALE000\Desktop\path.txt", "w") as file:
+    file.write("{} {} {}\n".format(b,coordsX, coordsY))
+    for x in range(0, len(data.vertices)):
+        coords = plain_verts[x]
+        coordsX = coords[0]
+        coordsY = coords[1]
+        file.write(f"\nCoords: {coordsX},{coordsY}")
