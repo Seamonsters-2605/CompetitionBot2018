0d4e28412cd5c963befdb58b9fd84ef690cce29a
Tollboothslyr <kazp.entire@gmail.com>
2018-11-08 19:16:54 -0800

    done!
    its done!
    
    oh yea this is the blender autonomous

--- auto_scripting/blender_export.py
+++ auto_scripting/blender_export.py
@@ -1,47 +1,36 @@
-import bpy
-#from setup import rotateWheels, 
-FRAME_STEP = 25
-#if rotateWheels = True:
-#wheels_obj = bpy.data.objects['theWheels']
-#else:
- #   wheels_obj = " "
-scene = bpy.data.scenes['Scene']
+import bpy, bmesh
+import math
 
-robot_obj = bpy.data.objects['TheRobot']
+path = bpy.data.objects['NurbsPath']
 
-robot_path = bpy.data.objects['NurbsPath']
+obj = bpy.context.active_object
 
-markers = sorted(scene.timeline_markers, key=lambda m: m.frame)
-marker_i = 0
-#set frame
-#pathTime = 0 #start pointe of curve. also keep marker_i and dab two different things.
-#when increasing from 100 to x end the curve end always needs to stay as >= 1 or <=100
-#bpy.data.objects['NurbsPath'].eval_time = pathTime	#beggining of curve path thing
+path.select = True
+data = obj.data
+bpy.context.scene.objects.active = path
 
-print(robot_obj.rotation_euler) #debugging
-robot_obj.select = True
-robot_path.select = True
-bpy.context.scene.objects.active = robot_obj
-#bpy.ops.object.parent_set(type='FOLLOW_PATH')
-empty = robot_obj.constraints.new('FOLLOW_PATH')
-empty.target = robot_path
-#robot_obj.constraints["FOLLOW_PATH"].use_curve_follow
-K = robot_obj.constraints["Follow Path"] #or "FollowPathConstraint.use_curve_follow
-K.offset = 10 #arbitrary amount it worked when i did ten so im using ten
-K.influence = 0 #the new path time
+
 for obj in bpy.data.objects:
     obj.select = False
-
-with open("C:\\Users\**YOURNAME000***\Desktop\path.txt", "w") as file:
-    for frame in range(scene.frame_start, scene.frame_end + FRAME_STEP, FRAME_STEP):
-        bpy.ops.object.paths_calculate()
-        if K.influence > 1:
-            K.influence -=.1 #check. when path time is more than 100 increment it down to keep it at 100 which is the max value
-        if marker_i < len(markers) and frame >= markers[marker_i].frame:
-            file.write("{} {}\n".format(markers[marker_i].frame, ":" + markers[marker_i].name))
-            marker_i += 1
-            K.influence += .1  # change for sensitivity/ speed of robot          
-        scene.frame_current = frame
-        # frame number, x, y , z Rotation
-        file.write("{} {} {} {}\n".format(frame, robot_obj.location[0], robot_obj.location[1], robot_obj.rotation_euler[2]))
+Num = 0
+with open("C:\\Users\PIROSKAZ000\Desktop\path.txt", "w") as file:
+    for x in range(0,len(obj.data.vertices)):      
+        print(len(obj.data.vertices))
+        if obj.mode == 'EDIT':
+            bm = bmesh.from_edit_mesh(obj.data)
+            vertices = bm.verts 
+        else:
+            vertices = obj.data.vertices
+        verts = [obj.matrix_world * vert.co for vert in vertices] 
+        plain_verts = [vert.to_tuple() for vert in verts]
+        print(plain_verts)
         
+        rotPoints = plain_verts[x-1]
+        coords = plain_verts[x];coords1 = plain_verts[x]
+        coordsX1 = rotPoints[0];coordsY1 = rotPoints[1]
+        coordsX = coords[0];coordsY = coords[1]
+        diffX = abs(coordsX-coordsX1);diffY = abs(coordsY-coordsY1)
+        Theta = math.atan2((diffY+.001),(diffX+.001))
+        
+        file.write("{} {} {}\n".format(coordsX, coordsY, Theta))
+  
